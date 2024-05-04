556f962949ec9e67a6403a0d9e61018cf8f1f383
muhammadrayhanwidayat <murayat99@gmail.com>
2024-05-04 14:16:13 +0700

    membuat fungsi search

--- double_linkedlist/double_linkedlist.cpp
+++ double_linkedlist/double_linkedlist.cpp
@@ -56,6 +56,16 @@ void addNode() {
             START = newNode;
         }
     }
+}bool search(int rollNo, Node** previous, Node** current)
+{
+    *previous = NULL;
+    *current = START;
+    while (*current != NULL && (*current)->noMhs != rollNo)
+    {
+        *previous = *current;
+        *current = (*current)->next;
+    }
+    return (*current != NULL);
 }
 
 
